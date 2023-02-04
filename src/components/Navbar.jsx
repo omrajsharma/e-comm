7f0a8394273b585694d6932ba55338897347d423
Omraj Sharma <omraj7413@gmail.com>
2023-02-04 10:07:53 +0530

    fix: navbar-brand link updated to router link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ function CollapsibleExample() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="/">Buy&Sell</Navbar.Brand>
+        <Link className='navbar-brand' to="/">Buy&Sell</Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
