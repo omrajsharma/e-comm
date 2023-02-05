8a02c5c078b8de09c88834d859bd681c9a7347bf
Omraj Sharma <omraj7413@gmail.com>
2023-02-05 10:42:16 +0530

    feat: recommendation section

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HomeBanner from '../molecules/HomeBanner'
 import ItermList from '../organisms/ItemList'
+import RecommendationList from '../organisms/RecommendationList'
 import './Home.css'
 
 function Home() {
   return (
     <div>
         <HomeBanner/>
+        <RecommendationList/>
         <ItermList />
     </div>
   )
---
+++ src/components/organisms/RecommendationList.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import axios from 'axios'
+import './RecommendationList.css'
+import ItemCard from '../molecules/ItemCard'
+import { Container } from 'react-bootstrap';
+
+function RecommendationList() {
+
+  const [itemsList, setItemsList] = React.useState([]);
+  React.useEffect( () => {
+    // Get API call using axios
+    axios.get("https://buy-and-sell-f5fe8-default-rtdb.asia-southeast1.firebasedatabase.app/item-list.json")
+      .then(response => {
+        let itemsList = [];
+        for (let key in response.data) {
+          itemsList.push(response.data[key])
+        }
+        setItemsList(itemsList)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
+  }, [] )
+
+
+  return (
+    <div>
+        <Container>
+            <section className='recommendation-section'>
+                <h4 className="recommendation-list-title">
+                    <span>Recommendations</span>
+                </h4>
+                <div className="recommendation-list">
+                    <div className="recommendation-list-slider">
+                        {
+                            itemsList.length === 0 
+                            ? <h3>No items found</h3>
+                            : itemsList.map((item, index) => (
+                                <ItemCard
+                                key={index}
+                                itemImg={item.itemImg}
+                                itemPrice={item.itemPrice}
+                                itemTitle={item.itemTitle}
+                                itemDesc={item.itemDesc}
+                                itemPlace={item.itemPlace}
+                                itemDate={item.itemDate}
+                                />
+                            ))
+                        }
+                    </div>
+                </div>
+            </section>
+        </Container>
+    </div>
+  )
+}
+
+export default RecommendationList
