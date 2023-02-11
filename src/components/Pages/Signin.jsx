f103d1741e0b76f671b8c36e815534c51dffa98d
Omraj Sharma <omraj7413@gmail.com>
2023-02-11 11:32:28 +0530

    feat: sign in component

--- src/components/Pages/Signin.jsx
+++ src/components/Pages/Signin.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './Signin.css'
 
 function Signin() {
   return (
-    <div>Signin</div>
+    <Container>
+      <div className='signin-header'>
+        <h1>
+          Sign in
+        </h1>
+      </div>
+
+      <div className="signin-form">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </Container>
   )
 }
 
