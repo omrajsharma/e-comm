b3e60669cb3f42a64b7cdb22253d80967d69a1e8
Omraj Sharma <omraj7413@gmail.com>
2023-02-11 11:41:19 +0530

    feat: signup form component

--- src/components/Pages/Signup.jsx
+++ src/components/Pages/Signup.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './Signup.css'
 
 function Signup() {
   return (
-    <div>Signup</div>
+    <Container>
+      <div className='signup-header'>
+        <h1>
+          Sign up
+        </h1>
+      </div>
+
+      <div className="signup-form">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter name" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" placeholder="Enter username" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPhoneNumber">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control type="number" placeholder="Enter phone number" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </Container>
   )
 }
 
