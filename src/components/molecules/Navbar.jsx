065b6b502fb38ba67313d9eb4b58e3713abf55ab
Omraj Sharma <omraj7413@gmail.com>
2023-02-18 11:17:09 +0530

    feat: auto updation of cart pill in navbar

--- src/components/molecules/Navbar.jsx
+++ src/components/molecules/Navbar.jsx
@@ -10,7 +10,7 @@ import {
 } from "react-router-dom";
 import { Badge } from 'react-bootstrap';
 
-function CollapsibleExample() {
+function CollapsibleExample({cartList}) {
   return (
     <Navbar id='navbar' collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
@@ -32,7 +32,7 @@ function CollapsibleExample() {
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
               </svg>
               <Badge pill>
-                2
+                {cartList.length}
               </Badge>
             </Link>
           </Nav>
